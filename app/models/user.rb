349f4f52476645183d7bf32c00250ce9be25e684
Dylan Palmboom <nofxboy1234@gmail.com>
2024-10-24 08:53:23 +0200

    Refactor User query

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
   has_many :friendships,
-   ->(user) { FriendshipsQuery.both_ways(user_id: user.id) },
+   ->(user) {
+    relationn = @relation ||= Friendship.all
+    relationn.unscope(where: :user_id)
+    .where(user_id: user.id)
+    .or(relationn.where(friend_id: user.id))
+
+     # FriendshipsQuery.both_ways(user_id: user.id)
+   },
    inverse_of: :user,
    dependent: :destroy
 

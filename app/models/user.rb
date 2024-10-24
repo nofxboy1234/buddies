d345d84ccb1d1867c7b4d88a5a9791930f75fc55
Dylan Palmboom <nofxboy1234@gmail.com>
2024-10-24 23:09:50 +0200

    Rearrange query lines

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,10 +14,24 @@ class User < ApplicationRecord
       @relation ||= User.all
       query = @relation.joins("OR users.id = friendships.user_id")
 
-      query = query.where.not(id: user.id)
+      query = query.where(friendships: { user_id: user.id })
+      .or(query.where(friendships: { friend_id: user.id }))
 
-      query.where(friendships: { user_id: user.id })
-        .or(query.where(friendships: { friend_id: user.id }))
+      query = query.where.not(id: user.id)
     },
     through: :friendships
 end
+
+# SELECT "users".* FROM "users"
+
+# INNER JOIN "friendships" ON
+# "users"."id" = "friendships"."friend_id"
+# OR
+# users.id = friendships.user_id
+
+# WHERE
+# ("friendships"."user_id" = $1 OR "friendships"."friend_id" = $2)
+# AND
+# "users"."id" != $3
+
+# [["user_id", 1], ["friend_id", 1], ["id", 1]]

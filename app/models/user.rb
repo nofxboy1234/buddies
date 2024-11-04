1156c973299a80a2407b82f9a67e45b92f0c2af8
Dylan Palmboom <nofxboy1234@gmail.com>
2024-11-04 14:28:57 +0200

    Label query pieces

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,25 +3,37 @@ class User < ApplicationRecord
    ->(user) {
     friendships = Friendship.unscope(where: :user_id)
     query1 = friendships.where(user_id: user.id)
-    query2 = Friendship.where(friend_id: user.id)
+    query2 = Friendship.where(friend_id: user.id) # {1}
     query1.or(query2)
    },
    inverse_of: :user,
    dependent: :destroy
 
+  # SELECT "friendships".*
+  # FROM "friendships"
+  # WHERE ("friendships"."user_id" = 1 {1}OR "friendships"."friend_id" = 1{1})
+
   has_many :friends,
     ->(user) {
-      query1 = User.joins("OR users.id = friendships.user_id")
-        .where(friendships: { user_id: user.id })
-        .where.not(id: user.id)
+      query1 = User.joins("OR users.id = friendships.user_id") # {2}
+        .where(friendships: { user_id: user.id }) # {3}
+        .where.not(id: user.id) # {5}
 
-      query2 = User.joins("OR users.id = friendships.user_id")
-        .where(friendships: { friend_id: user.id })
-        .where.not(id: user.id)
+      query2 = User.joins("OR users.id = friendships.user_id") # {2}
+        .where(friendships: { friend_id: user.id }) # {4}
+        .where.not(id: user.id) # {5}
 
       query1.or(query2)
     },
     through: :friendships
+
+  # SELECT "users".*
+  # FROM "users"
+  # INNER JOIN "friendships"
+  # ON "users"."id" = "friendships"."friend_id" {2}OR users.id = friendships.user_id{2}
+  # WHERE
+  # ({3}"friendships"."user_id" = 1{3} {1}{4}OR "friendships"."friend_id" = 1{1})
+  # {5}AND "users"."id" != 1{5}
 end
 
 # SELECT "users".* FROM "users"

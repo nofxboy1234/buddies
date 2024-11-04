f6f44cec0e6424d423a431e6887c9b329cbbfcbb
Dylan Palmboom <nofxboy1234@gmail.com>
2024-11-04 15:42:31 +0200

    Update query labels

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,26 +2,26 @@ class User < ApplicationRecord
   has_many :friendships,
    ->(user) {
     friendships = Friendship.unscope(where: :user_id)
-    query1 = friendships.where(user_id: user.id) # {3}
-    query2 = Friendship.where(friend_id: user.id) # {1}
+    query1 = friendships.where(user_id: user.id) # {1}
+    query2 = Friendship.where(friend_id: user.id) # {2}
     query1.or(query2)
    },
    inverse_of: :user,
-   dependent: :destroy
+   dependent: :destroys
 
   # SELECT "friendships".*
   # FROM "friendships"
-  # WHERE ("friendships"."user_id" = 1 {1}OR "friendships"."friend_id" = 1{1})
+  # WHERE ({1}"friendships"."user_id" = 1{1} {2}OR "friendships"."friend_id" = 1{2})
 
   has_many :friends,
     ->(user) {
-      query1 = User.joins("OR users.id = friendships.user_id") # {2}
-        .where(friendships: { user_id: user.id }) # {3}
-        .where.not(id: user.id) # {5}
+      query1 = User.joins("OR users.id = friendships.user_id") # {3}
+        .where(friendships: { user_id: user.id }) # {4}
+        .where.not(id: user.id) # {6}
 
-      query2 = User.joins("OR users.id = friendships.user_id") # {2}
-        .where(friendships: { friend_id: user.id }) # {4}
-        .where.not(id: user.id) # {5}
+      query2 = User.joins("OR users.id = friendships.user_id") # {3}
+        .where(friendships: { friend_id: user.id }) # {5}
+        .where.not(id: user.id) # {6}
 
       query1.or(query2)
     },
@@ -30,8 +30,8 @@ class User < ApplicationRecord
   # SELECT "users".*
   # FROM "users"
   # INNER JOIN "friendships"
-  # ON "users"."id" = "friendships"."friend_id" {2}OR users.id = friendships.user_id{2}
+  # ON "users"."id" = "friendships"."friend_id" {3}OR users.id = friendships.user_id{3}
   # WHERE
-  # ({3}"friendships"."user_id" = 1{3} {1}{4}OR "friendships"."friend_id" = 1{1})
-  # {5}AND "users"."id" != 1{5}
+  # ({1}{4}"friendships"."user_id" = 1{1}{4} {2}{5}OR "friendships"."friend_id" = 1{2}{5})
+  # {6}AND "users"."id" != 1{6}
 end

4abceadc3b4c4db221e03acf35b4065862871a7a
Dylan Palmboom <nofxboy1234@gmail.com>
2024-11-05 11:43:18 +0200

    Add test instance method and class method to Book

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,12 @@ class Book < ApplicationRecord
   belongs_to :author
 
   scope :with_id_greater_than, ->(id) { where("id > ?", id) }
+
+  def hello
+    puts "hello"
+  end
+
+  def self.bye
+    puts "bye"
+  end
 end

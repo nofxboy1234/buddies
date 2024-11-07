c227e98da278611b2614570f674b23629ea099a6
Dylan Palmboom <nofxboy1234@gmail.com>
2024-11-07 10:13:18 +0200

    Test scopes with associations and joins

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,6 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: "User"
+
+  scope :today, -> { where("created_at > ?", Time.now.midnight) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,28 @@
 class User < ApplicationRecord
-  has_many :friendships,
-   ->(user) {
-    friendships = Friendship.unscope(where: :user_id)
-    query1 = friendships.where(user_id: user.id) # {1}
-    query2 = Friendship.where(friend_id: user.id) # {2}
-    query1.or(query2)
+  # In rails console:
+
+  # User.select('users.id', 'users.name', 'friendships.user_id', 'friendships.friend_id')
+  # .joins(:today_friendships)
+
+  # User.select("users.id", "users.name", "friendships.user_id", "friendships.friend_id")
+  # .joins(:friendships)
+  # .where('friendships.created_at > ?', 1.week.ago)
+
+  has_many :today_friendships,
+   -> {
+    where("friendships.created_at > ?", Time.now.midnight)
    },
+  inverse_of: :user,
+  class_name: "Friendship",
+  dependent: :destroy
+
+  has_many :friendships,
+   #  ->(user) {
+   #   friendships = Friendship.unscope(where: :user_id)
+   #   query1 = friendships.where(user_id: user.id) # {1}
+   #   query2 = Friendship.where(friend_id: user.id) # {2}
+   #   query1.or(query2)
+   #  },
    inverse_of: :user,
    dependent: :destroy
 
@@ -14,10 +31,10 @@ class User < ApplicationRecord
   # WHERE ({1}"friendships"."user_id" = 1{1} {2}OR "friendships"."friend_id" = 1{2})
 
   has_many :friends,
-    ->(user) {
-      User.joins("OR users.id = friendships.user_id") # {3}
-        .where.not(id: user.id) # {4}
-    },
+    # ->(user) {
+    #   User.joins("OR users.id = friendships.user_id") # {3}
+    #     .where.not(id: user.id) # {4}
+    # },
     through: :friendships
 
   # SELECT "users".*
